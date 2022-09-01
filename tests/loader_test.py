7d2f802c78c83120b55a235c252a1f7518f52c22
kismetd <hlevnmaxim@gmail.com>
2022-09-01 16:34:36 +0300

    tests: unit-tests for download() from loader.py

--- tests/loader_test.py
+++ tests/loader_test.py
@@ -1,5 +1,24 @@
+import tempfile
+from pathlib import Path
+
+import requests_mock
 from page_loader.loader import download
 
+URL = "https://ru.hexlet.io/courses"
+TEST_FILE = "raw.html"
 
-def test():
-    assert True == 1
+
+def get_fixture_path(file_name):
+    return str(Path.cwd() / "tests" / "fixtures" / file_name)
+
+
+def test_download_makes_request_and_saves_file():
+    fixture_path = get_fixture_path(TEST_FILE)
+    expected = Path(fixture_path).read_bytes()
+    with requests_mock.Mocker() as m:
+        m.get(URL, content=expected)
+        with tempfile.TemporaryDirectory() as tmpdir:
+            result_path = download(URL, tmpdir)
+            result = Path(result_path).read_bytes()
+
+    assert result == expected
