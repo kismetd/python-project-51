2f408f54ce86ee35f1714caa8554d20cc7ac4e62
kismetd <hlevnmaxim@gmail.com>
2022-09-06 20:22:44 +0300

    refactor: move exit codes to main script

--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -5,6 +5,7 @@ Download web pages locally
 import logging
 
 from page_loader.cli import get_args
+from page_loader.exceptions import FileSystemError, NetworkError
 from page_loader.loader import download
 from page_loader.logconf import setup
 
@@ -14,7 +15,10 @@ logger = logging.getLogger(__name__)
 def main():
     target_dir, url, log_lvl = get_args()
     setup(log_lvl)
-    print(download(url, target_dir))
+    try:
+        print(download(url, target_dir))
+    except (NetworkError, FileSystemError):
+        raise SystemExit(1)
 
 
 if __name__ == "__main__":
